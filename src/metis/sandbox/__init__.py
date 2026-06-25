92ef5471fb512066f90eac286fb1589f9d94036c
luke <onegqluke@icloud.com>
2026-06-25 09:56:42 -0400

    Added read actinos to init export

--- src/metis/sandbox/__init__.py
+++ src/metis/sandbox/__init__.py
@@ -7,6 +7,7 @@ Re-exports the scoped file tools and the lockbox violation type so callers can
 from __future__ import annotations
 
 from metis.sandbox.lockbox import LockboxViolation
+from metis.sandbox.runlog import read_actions
 from metis.sandbox.tools import list_dir, read_file, write_file
 
-__all__ = ["LockboxViolation", "read_file", "write_file", "list_dir"]
+__all__ = ["LockboxViolation", "read_file", "write_file", "list_dir", "read_actions"]
